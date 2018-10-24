486db8690233b4d7e5dde292655b988714cf61a7
sharafian <ben@sharafian.me>
2018-10-24 14:58:30 -0700

    feat: measure pulse on bucket

--- src/bucket.js
+++ src/bucket.js
@@ -1,6 +1,8 @@
 const stream = require('stream')
 const debug = require('debug')('web-monetization-receiver:bucket')
 const EventEmitter = require('events')
+const DEFAULT_THROUGHPUT = 100000
+const DEFAULT_WINDOW = 5000
 const DEFAULT_FREE_BYTES = 0
 const DEFAULT_COST_PER_BYTE = 1 / 5000
 
@@ -10,6 +12,10 @@ class Bucket {
     this.capacity = opts.capacity || Infinity
     this.balance = 0
     this.events = new EventEmitter()
+
+    // sliding window of payment events
+    this.window = []
+    this.pulse = 0
   }
 
   fund (amount) {
@@ -18,10 +24,27 @@ class Bucket {
       throw new Error('invalid amount. amount=' + amount)
     }
 
+    let total = 0
+    const now = Date.now()
+    this.window.push({ amount: n, date: now })
+    for (let i = 0; i < this.window.length; ++i) {
+      if (item.date + DEFAULT_WINDOW > now) {
+        this.window.splice(i, 1)
+        --i
+        continue
+      }
+      total += item.amount
+    }
+
+    this.pulse = total / (DEFAULT_THROUGHPUT * (DEFAULT_WINDOW / 1000))
     this.balance = Math.min(
       this.balance + n,
       this.capacity)
     this.events.emit('fund', this.balance)
+  }
+
+  pulse () {
+    return this.pulse
   }
 
   spend (amount) {
